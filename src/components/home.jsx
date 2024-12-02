0df3fa0262b4fe87301b2c483b20904e3c1488a5
somilvaishya <somilvaishya78@gmail.com>
2024-12-02 23:42:26 +0530

    update

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const home = () => {
+  return <div>home</divimport React from 'react';
+
+const Home = () => {
+  return <div>Home Page</div>;
+};
+
+export default Home>;
+};
+
+export default home;
